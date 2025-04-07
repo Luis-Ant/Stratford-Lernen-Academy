2e452bbc2b18da9714720698db78e578702f7549
Luis-Ant <luisamt311@gmail.com>
2025-04-07 17:45:39 -0600

    Se agrega la estructura para trabajar el login

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('¡El servidor está funcionando!');
+app.get("/", (req, res) => {
+  res.send("¡El servidor está funcionando!");
 });
 
 app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
